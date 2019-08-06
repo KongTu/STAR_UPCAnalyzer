33dfdc3670e65d095b589eb98cb7f3d21ef96a84
Zhoudunming Tu <kong@130-199-16-253.dhcp.bnl.gov>
2019-08-06 14:24:12 -0400

    change y2013

---
+++ StRoot/StTriggerUtilities/StDSMUtilities/BOARDAlgo.hh
@@ -0,0 +1,11 @@
+#ifndef BOARD_ALGO_HH
+#define BOARD_ALGO_HH
+
+struct Board;
+
+struct BoardAlgo {
+  virtual void operator()(Board& dsm) = 0;
+  virtual ~BoardAlgo() {}
+};
+
+#endif	// BOARD_ALGO_HH
