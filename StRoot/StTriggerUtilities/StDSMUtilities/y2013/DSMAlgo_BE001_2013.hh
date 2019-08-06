b6bf2f3e1f1bbd87a9046dacc0660aa23c50e78a
Zhoudunming Tu <kong@130-199-16-253.dhcp.bnl.gov>
2019-08-06 14:30:03 -0400

    change y2013

--- StRoot/StTriggerUtilities/StDSMUtilities/y2013/DSMAlgo_BE001_2013.hh
+++ StRoot/StTriggerUtilities/StDSMUtilities/y2013/DSMAlgo_BE001_2013.hh
@@ -2,14 +2,9 @@
 #define DSM_ALGO_BE001_2013_HH
 
 #include "../DSMAlgo.hh"
-#include "../Board.hh"
 
-// struct DSMAlgo_BE001_2013 : public DSMAlgo {
-//   void operator()(DSM& dsm);
-// };
-
-struct Board;
-
-void DSMAlgo_BE001_2013(Board& dsm)
+struct DSMAlgo_BE001_2013 : public DSMAlgo {
+  void operator()(DSM& dsm);
+};
 
 #endif	// DSM_ALGO_BE001_2013_HH
