226c749916312bcd9f34f1098f1651f3e5bfbe86
Zhoudunming Tu <kong@130-199-16-253.dhcp.bnl.gov>
2019-08-06 10:40:28 -0400

    change UPCtree

--- StRoot/StUPCTreeMaker/StUPCTreeMaker.cxx
+++ StRoot/StUPCTreeMaker/StUPCTreeMaker.cxx
@@ -107,6 +107,7 @@ Int_t StUPCTreeMaker::Make()
     mBemcTriggerSimu = 0;
     mTriggerSimuMaker = (StTriggerSimuMaker*) new StTriggerSimuMaker("StarTrigSimu");
     mTriggerSimuMaker->setMC(0);
+    mTriggerSimuMaker->useOfflineDB();
     mTriggerSimuMaker->useBbc();
     mTriggerSimuMaker->useBemc();
 
